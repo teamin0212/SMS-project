23066983c298a10492c5654330397eb38f76807a
USER <jirr1010@gmail.com>
2018-09-01 16:10:43 +0900

    SMS

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,26 +23,28 @@ public class MainActivity extends AppCompatActivity implements Gota.OnRequestPer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Gota.Builder(this)
-                .withPermissions(Manifest.permission.CALL_PHONE)
+                .withPermissions(Manifest.permission.SEND_SMS)
                 .requestId(1)
                 .setListener(this)
                 .check();
         final EditText phone_number = (EditText)findViewById(R.id.editText);
+        final EditText text = (EditText)findViewById(R.id.editText2);
         Button button = (Button)findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tei : "+phone_number.getText().toString()));
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts(text.getText().toString(), phone_number.getText().toString(), null));
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
                     startActivity(intent);
+                    SmsManager.getDefault().sendTextMessage(phone_number.getText().toString(), null, text.getText().toString(), null, null);
                 }
             }
         });
     }
     @Override
     public void onRequestBack(int requestId, @NonNull GotaResponse gotaResponse) {
-        if(gotaResponse.isGranted(Manifest.permission.CAMERA)) {
+        if(gotaResponse.isGranted(Manifest.permission.SEND_SMS)) {
             // Your Code
         }
     }
